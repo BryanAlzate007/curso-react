9362e1ef92a05547d9f7caeaaa2c080a67fabb47
Bryan Alzate <bryanalzate007@gmail.com>
2024-03-31 13:26:20 -0500

    se cargar esturctura de proyecto

---
+++ src/App/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+import { useLocalStorage } from './useLocalStorage';
+import { AppUI } from './AppUI';
+
+// const defaultTodos = [
+//  { text: 'Cortar cebolla', completed: true },
+//  { text: 'Tomar el Curso de Intro a React.js', completed: false },
+//  { text: 'Llorar con la Llorona', completed: false },
+//  { text: 'LALALALALA', completed: false },
+//  { text: 'Usar estados derivados', completed: true },
+//];
+
+
+
+function App() {
+
+
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(
+    todo => !!todo.completed
+  ).length;
+  const totalTodos = todos.length;
+
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    }
+  );
+
+ 
+
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex( // eslint-disable-next-line
+      (todo) => todo.text == text
+    );
+    newTodos[todoIndex].completed = true;
+    saveTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(// eslint-disable-next-line
+      (todo) => todo.text == text
+    );
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
+  };
+  
+  return (
+    <AppUI 
+    
+    completedTodos={completedTodos}
+    totalTodos={totalTodos}
+    searchValue={searchValue}
+    setSearchValue={setSearchValue}
+    searchedTodos={searchedTodos}
+    completeTodo={completeTodo}
+    deleteTodo={deleteTodo}
+    
+    />
+
+  );
+}
+
+export default App;
