bd1f5ea796193c7de7c2afdce18785ba7ca22afd
Bryan Alzate <bryanalzate007@gmail.com>
2024-03-31 23:55:28 -0500

    se carga la funsiones de crear TODO

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
@@ -7,21 +8,23 @@ import { TodosError } from '../TodosError';
 import { EmptyTodos } from '../EmptyTodos';
 import { CreateTodoButton } from '../CreateTodoButton';
 import { TodoContext } from '../TodoContext';
+import { Modal } from '../Modal';
+import { TodoForm } from '../TodoForm';
 
 function AppUI(){
+  const {  loading,
+    error,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+  } = React.useContext(TodoContext);
     return (
         <>
         <TodoCounter/>
         <TodoSearch />
-        <TodoContext.Consumer>
-          {({
-             loading,
-             error,
-             searchedTodos,
-             completeTodo,
-             deleteTodo,
-          }) => (
-              <TodoList>
+            <TodoList>
               {loading && <TodosLoading />}
               {error && < TodosError />}
               {(!loading && searchedTodos.length === 0) && <EmptyTodos />}
@@ -37,12 +40,17 @@ function AppUI(){
                 />
               ))}
             </TodoList>
-          )
+        <CreateTodoButton 
+          setOpenModal={setOpenModal}
+        />
 
-          }
-          
-        </TodoContext.Consumer>
-        <CreateTodoButton />
+
+        {openModal && (
+            <Modal>
+                <TodoForm />
+            </Modal>
+        )}
+      
       </>
     );
 }
--- src/TodoContext/index.js
+++ src/TodoContext/index.js
@@ -12,6 +12,7 @@ function TodoProvider({children}){
         error,
       } = useLocalStorage('TODOS_V1', []);
       const [searchValue, setSearchValue] = React.useState('');
+      const [openModal, setOpenModal] = React.useState(false);
     
       const completedTodos = todos.filter(
         todo => !!todo.completed
@@ -26,7 +27,15 @@ function TodoProvider({children}){
         }
       );
     
-     
+     const addTodo = (text) => {
+      const newTodos = [...todos];
+      newTodos.push({
+        text,
+        completed: false,
+      });
+      saveTodos(newTodos);
+     };
+
     
       const completeTodo = (text) => {
         const newTodos = [...todos];
@@ -58,6 +67,9 @@ function TodoProvider({children}){
           searchedTodos,
           completeTodo,
           deleteTodo,
+          openModal,
+          setOpenModal,
+          addTodo,
         }}>
           {children}
         </TodoContext.Provider>
