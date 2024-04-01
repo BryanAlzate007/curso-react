b1e82d58baf5736b0cc955f535b006ba14b55279
Bryan Alzate <bryanalzate007@gmail.com>
2024-03-31 19:48:12 -0500

    se continua organizando estrutura del proyecto

--- src/App/index.js
+++ src/App/index.js
@@ -16,7 +16,12 @@ import { AppUI } from './AppUI';
 function App() {
 
 
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error,
+  } = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(
@@ -55,6 +60,8 @@ function App() {
   return (
     <AppUI 
     
+    loading={loading}
+    error={error}
     completedTodos={completedTodos}
     totalTodos={totalTodos}
     searchValue={searchValue}
